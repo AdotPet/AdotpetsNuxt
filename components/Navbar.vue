b847daa5af6ebc440bbee44607a0fb9b57161246
Thiago Morette <thiagomorette@gmail.com>
2019-08-17 15:30:41 -0300

    style(component navbar): make navbar more responsive
    changed the CSS to make NavBar more responsive to monitors with low resolution

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,9 +6,7 @@
       <NavbarButton text="Adotar" />
       <NavbarButton text="Dicas" />
       <NavbarButton text="Sobre Nós" />
-    </div>
-    <div class="access-button">
-      <NavbarButton text="Acessar" $style="-access" />
+      <NavbarButton text="Acessar" $style="-login" />
     </div>
   </div>
 </template>
@@ -26,29 +24,27 @@ export default {
 <style scoped>
 .navbar-container {
   width: 100vw;
-  height: 80px;
+  height: 7vh;
   background-color: #ebf4ff;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.05), 0px 10px 15px rgba(0, 0, 0, 0.1);
 }
 
-.navbar-container > .buttons {
+.navbar-container .buttons {
+  /* background-color: #fff; */
+  height: 100%;
+  width: 70%;
   display: flex;
-  width: 60%;
-  justify-content: space-between;
   align-items: center;
+  justify-content: center;
   margin: auto;
 }
 
-.navbar-button {
-  width: 120px;
-  height: 50px;
-  line-height: 45px;
-  margin-top: 15px;
+.navbar-container .buttons .navbar-button {
+  margin-right: 10px;
 }
 
-.navbar-button.-access {
-  position: absolute;
-  right: 45px;
-  top: 0;
+.navbar-container .buttons .navbar-button.-login {
+  position: absolute;  
+  right: 5px;
 }
+
 </style>
